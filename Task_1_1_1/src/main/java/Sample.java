57920480f1c145d5942ff94bd34d15032b99e93d
Antonio <tcoy210@gmail.com>
2023-09-09 22:13:03 +0700

    Set environment for task

---
+++ Task_1_1_1/src/main/java/Sample.java
@@ -0,0 +1,18 @@
+/**
+ * Sample class to simulate 2.1 task functionality
+ */
+public class Sample {
+    /**
+     * 
+     */
+    public static void printHelloWorld() {
+        System.out.println("Hello world!");
+    }
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        printHelloWorld();
+    }
+}
