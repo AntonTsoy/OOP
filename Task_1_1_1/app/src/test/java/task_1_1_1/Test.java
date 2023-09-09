9446ab69a9a92a00803efec2b803381ff934a56f
Antonio <tcoy210@gmail.com>
2023-09-09 18:36:45 +0700

    testTestTesttest

--- Task_1_1_1/app/src/test/java/task_1_1_1/Test.java
+++
@@ -1,5 +0,0 @@
-package task_1_1_1;
-
-public @interface Test {
-
-}
--- Task_1_2_1/src/main/java/ru/nsu/shadrina/Sample.java
+++ Task_1_2_1/src/main/java/ru/nsu/shadrina/Sample.java
@@ -1,7 +1,7 @@
 package ru.nsu.shadrina;
 
 /**
- * Sample class to simulate 1.1 task functionality
+ * Sample class to simulate 2.1 task functionality
  */
 public class Sample {
     public static void printHelloWorld() {
