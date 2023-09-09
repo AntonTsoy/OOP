bfc03d32f510a388cf8aaf78c40c59f5f4da7dd0
AntonTsoy <AntonTsoy@users.noreply.github.com>
2023-09-09 12:08:15 +0000

    Deploying to gh-pages from @ AntonTsoy/OOP@26ebe142cc4a42009b2e6c3161674009d32203d5 🚀

---
+++ Pattern/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"Sample","l":"main(String[])","url":"main(java.lang.String[])"},{"p":"<Unnamed>","c":"Sample","l":"printHelloWorld()"},{"p":"<Unnamed>","c":"Sample","l":"Sample()","url":"%3Cinit%3E()"}]
