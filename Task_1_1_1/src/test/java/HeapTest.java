275044bed8eb64f0c2aabb42536f4dbd49dee853
Antonio <tcoy210@gmail.com>
2023-09-13 16:43:21 +0700

    Replaced import strings

--- Task_1_1_1/src/test/java/HeapTest.java
+++ Task_1_1_1/src/test/java/HeapTest.java
@@ -1,7 +1,8 @@
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
 
 import org.junit.jupiter.api.Test;
 
