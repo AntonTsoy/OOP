d6b895db44a0a6ae50697f5669c6cecf9fcc136a
Antonio <tcoy210@gmail.com>
2023-09-13 11:25:29 +0700

    Update .gitignore and etc

--- Task_1_1_1/src/main/java/CheckSort.java
+++ Task_1_1_1/src/main/java/CheckSort.java
@@ -1,4 +1,13 @@
+/**
+ * Entry-point class.
+ */
 public class CheckSort {
+
+    /**
+     * Static entry method.
+     *
+     * @param args Cmd args.
+     */
     public static void main(String[] args){
         Heap heap = new Heap(new int[] {5, 4, 3, 2, 1});
         heap.heapSort();
