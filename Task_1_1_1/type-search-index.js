43ccb34de8490db0b1c4fcc045fcba77820d64df
AntonTsoy <AntonTsoy@users.noreply.github.com>
2023-09-14 15:51:58 +0000

    Deploying to gh-pages from @ AntonTsoy/OOP@05bbd82f517ce8485a252263bbb33e429ee45116 🚀

--- Task_1_1_1/type-search-index.js
+++ Task_1_1_1/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"CheckSort"},{"p":"<Unnamed>","l":"Heap"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"CheckSort"},{"p":"<Unnamed>","l":"HeapSort"}]
