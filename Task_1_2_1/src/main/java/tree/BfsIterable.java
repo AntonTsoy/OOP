c1f5f5513ee9ea21aaf76d2ebb53af3b0a44ded4
Antonio <tcoy210@gmail.com>
2023-10-16 09:32:58 +0700

    Fixed second reports

---
+++ Task_1_2_1/src/main/java/tree/BfsIterable.java
@@ -0,0 +1,77 @@
+package tree;
+
+import java.util.ArrayDeque;
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+
+/**
+ * Provides an iterable interface for breadth-first traversal of a Tree.
+ */
+public class BfsIterable<T> implements Iterable<T> {
+
+    /**
+     * Implements an iterator interface for breadth-first traversal of a Tree.
+     */
+    private class BfsIterator implements Iterator<T> {  // Can I make this class STATIC?
+
+        // Queue used to store nodes in breadth-first order.
+        public ArrayDeque<Tree<T>> BfsQueue;
+
+        /**
+         * Constructor. Initializes the queue with the root node.
+         */
+        public BfsIterator() {
+            BfsQueue = new ArrayDeque<Tree<T>>();
+            BfsQueue.add(treeNode);
+        }
+
+        /**
+         * Returns true if the iteration has more elements. 
+         * In other words, returns true if next() would return an element rather than throwing an exception.
+         *
+         * @return true if the iteration has more elements.
+         */
+        @Override
+        public boolean hasNext() {
+            return !BfsQueue.isEmpty();
+        }
+
+        /**
+         * Returns the next element in the iteration.
+         *
+         * @return the next element in the iteration.
+         */
+        @Override
+        public T next() {
+            Tree<T> currentNode = BfsQueue.pollFirst();
+            if (currentNode.getNodeValue() == null) {
+                throw new ConcurrentModificationException();
+            }
+
+            BfsQueue.addAll(currentNode.nodeChildren);
+            return currentNode.getNodeValue();
+        }
+    }
+
+    // The tree to traverse.
+    private final Tree<T> treeNode; 
+
+    /**
+     * Constructor.
+     *
+     * @param treeNode The tree to traverse.
+     */
+    public BfsIterable(Tree<T> treeNode) {
+        this.treeNode = treeNode;
+    }
+
+    /**
+     * Returns an iterator over elements of type T.
+     *
+     * @return an Iterator.
+     */
+    @Override
+    public Iterator<T> iterator() {
+        return new BfsIterator();
+    }
+}
