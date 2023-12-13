e10a48c16e47f0eaf5916c41c0b25e88eb05e9b6
AntonTsoy <AntonTsoy@users.noreply.github.com>
2023-12-13 10:44:55 +0000

    Deploying to gh-pages from @ AntonTsoy/OOP@a7d48f7517e20a98a67a7eaa10219aea67fff866 🚀

--- Task_1_5_1/package-search-index.js
+++ Task_1_5_1/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"calculator"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"calculator"},{"l":"calculator.exceptions"},{"l":"calculator.operations"}]
