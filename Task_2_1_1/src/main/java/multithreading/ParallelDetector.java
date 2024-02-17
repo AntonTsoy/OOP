7a431619c87bca18f265f48e199781604d51637c
Antonio <tcoy210@gmail.com>
2024-02-17 22:01:43 +0700

    Wrote template for parallel solution

---
+++ Task_2_1_1/src/main/java/multithreading/ParallelDetector.java
@@ -0,0 +1,41 @@
+package multithreading;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Класс, реализующий параллельное решение.
+ */
+public class ParallelDetector implements PrimeNumbersDetector {
+
+    @Override
+    public boolean isNotPrimeNumbers(Integer[] numbers) {
+        List<Integer> numList = new ArrayList<>(Arrays.asList(numbers));
+        return numList.parallelStream().anyMatch(num -> !PrimeNumbersDetectorUtils.isPrimeNumber(num));
+    }
+
+    public static void main(String[] args) {
+        ParallelDetector solution1 = new ParallelDetector();
+        
+        Integer[] numbers = new Integer[500000];
+        Integer num = 2;
+        int counter = 0;
+        while (counter < 500000) {
+            if (PrimeNumbersDetectorUtils.isPrimeNumber(num)) {
+                numbers[counter] = num;
+                counter++;
+            }
+            num++;
+        }
+
+        long startTime = System.currentTimeMillis();
+
+        boolean answer = solution1.isNotPrimeNumbers(numbers);
+
+        long endTime = System.currentTimeMillis();
+        long executionTime = endTime - startTime;
+        System.out.println("Time working: " + executionTime + " ms");
+        System.out.println(answer);
+    }
+}
