9edfafcbe1764b5c89fa72e03459f888a70332e3
Antonio <tcoy210@gmail.com>
2024-02-17 13:38:36 +0700

    Wrote the template for multithreading

--- Task_2_1_1/src/main/java/multithreading/ThreadedDetector.java
+++ Task_2_1_1/src/main/java/multithreading/ThreadedDetector.java
@@ -9,18 +9,62 @@ import java.util.concurrent.atomic.AtomicBoolean;
 public class ThreadedDetector implements PrimeNumbersDetector {
 
     private final int threadQuantity;
+    private Integer[] numbers;
 
     public ThreadedDetector(int threadQuantity) {
         this.threadQuantity = threadQuantity;
     }
 
-    private Thread newThread(ArrayList<Integer> numbers, AtomicBoolean result) {
+    private Thread createThread(int offset, int len, AtomicBoolean result) {
+        var newThread = new Thread(
+            () -> {
+                for (int idx = offset; idx < offset + len; idx++) {
+                    if (!PrimeNumbersDetectorUtils.isPrimeNumber(this.numbers[idx])) {
+                        result.set(true);
+                        break;
+                    }
+                }
+            });
         
+        newThread.setUncaughtExceptionHandler(
+            (thread, exception) -> {
+                synchronized (System.err) {
+                    System.err.println("Thread with offset - " + offset + ":");
+                    exception.printStackTrace(System.err);
+                    // попробуй здесь бросить исключение
+                    // https://stackoverflow-com.translate.goog/questions/6546193/how-to-catch-an-exception-from-a-thread?_x_tr_sl=en&_x_tr_tl=ru&_x_tr_hl=ru&_x_tr_pto=sc
+                }
+            }
+        );
+        return newThread;
     }
 
     @Override
     public boolean isNotPrimeNumbers(Integer[] numbers) {
+        if (numbers.length == 0) {
+            return true;
+        }
 
-        return false;
+        this.numbers = numbers;
+        AtomicBoolean result = new AtomicBoolean(false);
+        final Thread[] threads = new Thread[threadQuantity];
+        for (int threadIdx = 0; threadIdx < threadQuantity; threadIdx++) {
+            int offset = TaskDelimiter.offsetThreadPart(threadIdx, threadQuantity, numbers.length);
+            int len = TaskDelimiter.lenThreadPart(threadIdx, threadQuantity, numbers.length);
+            threads[threadIdx] = createThread(offset, len, result);
+
+            threads[threadIdx].start();
+        }
+
+        try {
+            // тут главный поток ждёт, но можно его тоже загрузить
+            for (int threadIdx = 0; threadIdx < threadQuantity; threadIdx++) {
+                threads[threadIdx].join();
+            }
+        } catch (InterruptedException e) {
+            throw new RuntimeException("It is impossible situation!");
+        }
+
+        return result.get();
     }
 }
