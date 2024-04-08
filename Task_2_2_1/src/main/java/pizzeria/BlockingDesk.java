a7500e2815a4b9479ece95fe41edbde113370324
AntonTsoy <tcoy210@gmail.com>
2024-04-08 11:42:51 +0700

    Implemented my own blocking deque

--- Task_2_2_1/src/main/java/pizzeria/BlockingDesk.java
+++ Task_2_2_1/src/main/java/pizzeria/BlockingDesk.java
@@ -5,9 +5,41 @@ import java.util.ArrayDeque;
 
 public class BlockingDesk {
     
-    private ArrayDeque<Order> blockingDesk;
+    private ArrayDeque<Order> desk;
+    private int limit;
 
     public BlockingDesk() {
+        this.limit = -1;
+        this.desk = new ArrayDeque<Order>();
+    }
 
+    public BlockingDesk(int deskLimit) {
+        this.limit = deskLimit;
+        this.desk = new ArrayDeque<Order>();
+    }
+
+
+    public void push(Order newOrder) throws InterruptedException {
+        synchronized (this.desk) {
+            while (this.limit > 0 && this.desk.size() >= this.limit) {
+                this.desk.wait();
+            }
+            desk.add(newOrder);
+            desk.notify();
+        }
+    }
+
+    public Order pop() throws InterruptedException {
+        Order takenOrder;
+
+        synchronized (this.desk) {
+            while (this.desk.size() == 0) {
+                this.desk.wait();
+            }
+            takenOrder = desk.poll();
+            desk.notify();
+        }
+
+        return takenOrder;
     }
 }
