9cd56a2c297cbf504e9f87ef3aaa8de2e0d5c334
AntonTsoy <tcoy210@gmail.com>
2024-04-30 23:12:55 +0700

    ...

---
+++ Task_2_3_1/src/test/java/snake/Testing.java
@@ -0,0 +1,15 @@
+package snake;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+
+class Testing {
+
+    @Test
+    void checkMain() {
+        assertTrue(true);
+    }
+}
