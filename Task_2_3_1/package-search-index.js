1246a022d7cce7e2ba0fea91d31cf7147f460328
AntonTsoy <AntonTsoy@users.noreply.github.com>
2024-05-07 16:51:36 +0000

    Deploying to gh-pages from @ AntonTsoy/OOP@79935b9f8013f45181ee93af64b1e3b0cbaa6696 🚀

--- Task_2_3_1/package-search-index.js
+++ Task_2_3_1/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"fxlab.snake","l":"fxlab.snake"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"fxlab.snake","l":"fxlab.snake"},{"m":"fxlab.snake","l":"fxlab.snake.model"}];updateSearchResults();
